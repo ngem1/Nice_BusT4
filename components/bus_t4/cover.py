8d4af28c88136f9aeb8202695575dd5c37b16701
ngem1 <77919455+ngem1@users.noreply.github.com>
2025-01-21 21:00:34 +0000

    Update cover.py

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -1,9 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
-from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_ADDRESS
+from esphome.const import (
+    CONF_ADDRESS,
+    CONF_ID,
+    CONF_USE_ADDRESS,
+    CONF_TX_PIN,
+    CONF_RX_PIN,
+)
 
-
+CONF_UART_NUM = "uart_num"
 
 bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
 Nice = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
@@ -12,7 +18,9 @@ CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Nice),
     cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
-#    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_TX_PIN): cv.int_range(min=0, max=39),
+    cv.Optional(CONF_RX_PIN): cv.int_range(min=0, max=39),
+    cv.Optional(CONF_UART_NUM): cv.one_of("UART_NUM_0", "UART_NUM_1", "UART_NUM_2", "UART0", upper=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -29,8 +37,14 @@ def to_code(config):
     if CONF_USE_ADDRESS in config:
         use_address = config[CONF_USE_ADDRESS]
         cg.add(var.set_from_address(use_address))
+
+    # Handle UART settings
+    if CONF_TX_PIN in config:
+        cg.add(var.set_tx_pin(config[CONF_TX_PIN]))
         
+    if CONF_RX_PIN in config:
+        cg.add(var.set_rx_pin(config[CONF_RX_PIN]))
         
- #   if CONF_UPDATE_INTERVAL in config:
- #       update_interval = config[CONF_UPDATE_INTERVAL]
- #       cg.add(var.set_update_interval(update_interval))
+    if CONF_UART_NUM in config:
+        uart_num = config[CONF_UART_NUM]
+        cg.add(var.set_uart_num(uart_num))
