dbdc6286dc6176449e77795b996f9621cd24c153
pruwait <pruwait@gmail.com>
2025-08-13 22:54:08 +0300

    Update cover.py

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -7,48 +7,10 @@ from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_
 
 bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
 Nice = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
-
-# Old pattern
-#CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
-#{
-#    cv.GenerateID(): cv.declare_id(Nice),
-#    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
-#    cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
-##    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
-#}
-#).extend(cv.COMPONENT_SCHEMA)
-#### example
-#CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
-#    {
-#        cv.GenerateID(): cv.declare_id(RATGDOCover),
-#        cv.Optional(CONF_ON_OPENING): automation.validate_automation(
-#            {cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(CoverOpeningTrigger)}
-#        ),
-#        # ... other config options
-#    }
-#).extend(RATGDO_CLIENT_SCHEMA)
-###
-
-# New Pattern example
-#CONFIG_SCHEMA = (
-#    cover.cover_schema(RATGDOCover)  # Changed from COVER_SCHEMA to cover_schema()
-#    .extend(
-#        {
-#            cv.Optional(CONF_ON_OPENING): automation.validate_automation(
-#                {cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(CoverOpeningTrigger)}
-#            ),
-#            # ... other config options
-#        }
-#    )
-#    .extend(RATGDO_CLIENT_SCHEMA)
-#)
-
-# New Pattern
 CONFIG_SCHEMA = (
     cover.cover_schema(Nice)  # Changed from COVER_SCHEMA to cover_schema()
     .extend(
         {
-#    cv.GenerateID(): cv.declare_id(Nice),
     cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
 #    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
@@ -75,5 +37,6 @@ def to_code(config):
  #   if CONF_UPDATE_INTERVAL in config:
  #       update_interval = config[CONF_UPDATE_INTERVAL]
  #       cg.add(var.set_update_interval(update_interval))
+
 
 
